06ce82488081d455cfe8eedf4520b4b8f1470119
naxolorca <filorca@miuandes.cl>
2019-08-11 19:05:21 -0400

    fuentes caloricas

--- v008.py
+++ v008.py
@@ -12,11 +12,19 @@ dx = L / n  #discretizacion
 x = linspace(0, L, n+1)
 print x
 
+# Funcion para fuente calorica
+def q(k):
+    c = k/0.5
+    return c
+    
+
+
+
 #Creo el vector u_k para tiempo k con condicion inicial 1 en el tercio del medio
 
 u_k = zeros(len(x))
 for i in range(len(u_k)/3,2*len(u_k)/3):
-    u_k[i] = 1.
+    u_k[i] = 0.3
 
 #Temperatura en el tiempo k + 1
 u_km1 = u_k.copy()
@@ -48,8 +56,9 @@ for k in range(4000):
     if k < 10: #durante los primeros 10 segundos
     #u_k[0] = 0.
         for i in range(len(u_k)/3,2*len(u_k)/3): # en el tercio medio de la barra
-            # Se sube la energia e forma constante
-            u_km1[i]= u_k[i] + 1 + alpha*(u_k[i+1]-2*u_k[i]+u_k[i-1])  
+            # luego se le agrega cada vez mas energia, simulando que se genera cada vez mas calor en el tiempo
+            u_km1[i]= u_k[i] + q(k) + alpha*(u_k[i+1]-2*u_k[i]+u_k[i-1]) 
+
     
     # Loop en el espacio i = 1  ... n - 1 por las condiciones de borde
     for i in range(0,n+1):
