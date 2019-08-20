c7a9b7c3988451226e0600ae35c8ad4fa824e33a
naxolorca <filorca@miuandes.cl>
2019-08-20 17:21:29 -0400

    cambiar Nx problema overflow

--- Proyecto 1 2D otro clase.py
+++ Proyecto 1 2D otro clase.py
@@ -3,8 +3,8 @@ from matplotlib.pylab import *
 
 a = 1. #Ancho del dominio
 b = 1. #Largo del dominio
-Nx = 10 #Numero de intervalos en x
-Ny = 10 #Numero de intervalos en x
+Nx = 100 #Numero de intervalos en x
+Ny = 100 #Numero de intervalos en x
 
 dx = b/Nx # discretizacion espacial en x
 dy = a/Ny # discretizacion espacial en y
@@ -39,15 +39,15 @@ def printbien(u):
 print u_k
 
 def imshowbien(u):
-	imshow(u.T[Nx::-1,:])
+	imshow(u.T[Nx::-1,:],vmax=30)
 
 
 #Parametros de la barra (hormigon)
-dt = 1.  #s
-K = 1.63 # m^2 / s
-c = 880. # J / kg C
-rho = 2400. # kg/ m^3
-alpha = K*dt/(c*rho*dx**2)
+dt = 60.  #s
+K = 79.5 # m^2 / s
+c = 450. # J / kg C
+rho = 7800. # kg/ m^3
+alpha = 0.001#K*dt/(c*rho*dx**2)
 
 print "dt = ",dt 
 print "dx = ",dx
@@ -55,7 +55,7 @@ print "K = ",K
 print "c = ",c
 print "rho = ",rho
 print "alpha = ",alpha
-k_max=100000
+k_max=1000
 k= 0
 
 T = 24*3600 #perioddo
@@ -65,10 +65,15 @@ T = 24*3600 #perioddo
 #show()
 imshowbien(u_k)
 title("k = {} t = {} s".format(k, k*dt))
-#savefig("movie/frame_{0:04.0f}.png".format(k)) #guardo frame
-#colorbar()
-#close(1)
+savefig("movie/frame_{0:04.0f}.png".format(k)) #guardo frame
+colorbar()
+close(1)
 #Loop en el tiempo
+
+#CB inicial
+u_k[:,:] = 20.
+
+printbien(u_k)
 
 for k in range(k_max):
     t = dt*k
@@ -83,9 +88,9 @@ for k in range(k_max):
     u_k[:,-1] = u_ambiente = 20. + 10*sin((2*pi/T)*t)
 
     #if k <= k_max/4:
-    u_k[Nx/2,Ny/2] = 20.
+    
     # Loop en el espacio i = 1  ... n - 1 u_km1[0] = 0 u_km1[n] =  por las condiciones de borde
-    for i in range(0,Nx): #estaba Nx-1
+    for i in range(1,Nx): #estaba Nx-1
     	for j in range(0,Ny): #estaba Ny-1
 	        #algoritmo de diferencias finitas  2-D para difusion
 	        #Laplaciano
@@ -98,17 +103,20 @@ for k in range(k_max):
 
 
     #CB natural
-    #u_km1[Nx,:] = u_km1[Nx-1,:]
-    #u_km1[:,Ny] = u_km1[:,Ny-1]
+    u_km1[Nx,:] = u_km1[Nx-1,:]
+    u_km1[:,0] = u_km1[:,1]
+    u_km1[0,:] = u_km1[1,:]
+    
     # avanza la solucion a k +1
     
     u_k = u_km1
-imshowbien(u_k)
-title("k = {} t = {} s".format(k, k*dt))
-colorbar()
-show()
-    #savefig("movie/frame_{0:04.0f}.png".format(k)) #guardo frame
-    #close(1)
+#printbien(u_k)
+    imshowbien(u_k)
+    title("k = {} t = {} s".format(k, k*dt))
+    colorbar()
+#show()
+    savefig("movie/frame_{0:04.0f}.png".format(k)) #guardo frame
+    close(1)
 	
 
 
